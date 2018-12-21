81f366aa9fd774b6fe4d13e398034581a712058f
blurryface <jose.barealpz@gmail.com>
2018-12-21 10:32:22 +0100

    hibernate y act servcs

--- src/main/java/com/psicocare/persistence/ActivityManager.java
+++ src/main/java/com/psicocare/persistence/ActivityManager.java
@@ -1,5 +1,49 @@
 package com.psicocare.persistence;
 
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.psicocare.models.Activity;
+
+
+
 public class ActivityManager {
+	private static ActivityManager instance = null;
+
+	private static SessionFactory sfactory;
+
+	public static ActivityManager getInstance() {
+		if (instance == null) instance = new ActivityManager();
+		return instance;
+	}
+
+	private ActivityManager() {
+		sfactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+	}
+	
+	public List<Activity> getActivities() {
+		Session sess = sfactory.openSession();
+
+		List<Activity> listaActivity = sess.createQuery("from Activity").list();
+
+		sess.close();
+		return listaActivity;
+	}
+	
+
+	public Activity getActivityByAid(int aid) {
+		Session sess = sfactory.openSession();
+		
+		System.out.println("getActivityByAid:"+aid);
+		
+		Activity activity = sess.get(Activity.class, aid);
+		
+		
+		sess.close();
+		return activity;
+	}
 
 }
